b265c830ee5b9c6160804a776b46282381dd4015
Mickael JEGAT <moosic39@gmail.com>
2023-02-07 19:56:06 +0100

    add upload

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
 import { useState } from 'react';
-import reactLogo from './assets/react.svg';
 import './App.css';
 import Upload from './components/Upload';
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <div className='App'>
-      <h1>Vite + React</h1>
+      <h1>Jenn et Micka</h1>
+      <div>coucou</div>
       <Upload />
+      <Routes>
+        <Route></Route>
+      </Routes>
     </div>
   );
 }
--- src/components/Upload.tsx
+++ src/components/Upload.tsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 export default function Upload() {
   return (
-    <>
+    <div>
       insert photo here
       <input type='file' />
-    </>
+    </div>
   );
 }
